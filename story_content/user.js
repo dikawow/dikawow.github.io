c6910508f6f4a8573b7d11b7a01629b8fad5c934
dikawow <dikawow1130@gmail.com>
2021-09-30 22:01:02 +0700

    update1

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5lBdOHvoeTy":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  <input type="password" id="pswd" placeholder="pswd" />
 }
 
